f87044a00e8a97e72cee48877f951730a97b2c5a
koba789 <kobahide789@gmail.com>
2012-02-29 23:44:54 +0900

    remove unnecessary usecases

--- test/usecase.js
+++ test/usecase.js
@@ -1,36 +1,15 @@
-var flowering = require('../lib');
-var Nothing = flowering.Nothing;
-var Just = flowering.Just;
+var fs = require('fs');
+var flowering = require('../');
 
-var http = require('http');
-var url = require('url');
+var noop = function () {};
 
-var fetch = function (urlString, next) {
-  var options = url.parse(urlString);
-  
-  http.get(options, function(res) {
-    next(Just(res.body));
-  }).on('error', function(e) {
-    next(Nothing());
-  });
+var foo = fs.readFile.flower('/tmp/a.txt', 'utf-8');
+var bar = fs.readFile.flower('/tmp/b.txt', 'utf-8');
+
+var join = function (a, b, next) {
+  next(null, a.trim() + '-' + b.trim());
 };
 
-fetch = flowering.flower(fetch);
+var baz = join.flower(foo, bar);
 
-var a = fetch('http://koba789.com/storage/a.txt');
-var b = fetch('http://koba789.com/storage/b.txt');
-var c = fetch('http://koba789.com/storage/c.txt');
-
-a.show = function (val) {
-  console.log('a:' , val);
-};
-
-var concat = function (a, b, c) {
-  return Just([a, b, c].join(' '));
-};
-
-var joined = concat.flower(a, b, c);
-
-joined.show = function (val) {
-  console.log('joined:', val);
-};
+console.log.flower(baz).force();
